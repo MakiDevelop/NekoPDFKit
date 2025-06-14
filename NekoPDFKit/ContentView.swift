5b325f3763a061a2b01071628fb9c653e5cdf09d
MakiforDevelop <makifordevelop@gmail.com>
2025-06-14 19:14:01 +0800

    Initial Commit

---
+++ NekoPDFKit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NekoPDFKit
+//
+//  Created by 千葉牧人 on 2025/6/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NekoPDFKit/NekoPDFKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  NekoPDFKitApp.swift
+//  NekoPDFKit
+//
+//  Created by 千葉牧人 on 2025/6/14.
+//
+
+import SwiftUI
+
+@main
+struct NekoPDFKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
